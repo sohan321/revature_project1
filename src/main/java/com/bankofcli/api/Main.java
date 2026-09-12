8fe1e8ae02149bb7bbcc0961de9c9ea694fe1bcd
Sohan Shrestha <sohanomshrestha@gmail.com>
2026-09-12 16:46:43 -0500

    maven start

---
+++ src/main/java/com/bankofcli/api/Main.java
@@ -0,0 +1,9 @@
+package com.bankofcli.api;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bank of CLI - starting up...");
+        // TODO: wire up service layer and launch the menu loop
+    }
+}
